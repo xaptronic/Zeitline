2fdfb41c97fdd23dba4533421729b8c8769a007c
Alex Pilon <alex.pilon@shopify.com>
2024-10-25 15:45:57 -0400

    update to latest d3, latest webpack, add basic css to dist bundle

--- packages/zeitline/webpack.config.js
+++ packages/zeitline/webpack.config.js
@@ -1,32 +1,43 @@
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const production = process.env.NODE_ENV === 'production'; // eslint-disable-line
+const production = process.env.NODE_ENV === "production"; // eslint-disable-line
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: production ? './dist/zeitline.bundle.min.js' : './dist/zeitline.bundle.js',
-    library: 'Zeitline',
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "dist"),
+    filename: production ? "./zeitline.bundle.min.js" : "./zeitline.bundle.js",
+    library: "Zeitline",
+    libraryTarget: "umd",
   },
   module: {
     rules: [
       {
+        test: /\.css$/, // Matches any .css file
+        use: [
+          "style-loader", // Injects styles into DOM
+          "css-loader", // Turns CSS into CommonJS
+        ],
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules|test|dist)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['env'],
+            presets: ["@babel/preset-env"],
             cacheDirectory: true,
           },
         },
       },
     ],
   },
-  plugins: production ? [
-    new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
-  ] : [],
+  optimization: {
+    minimize: production, // Minimize only in production mode
+  },
+  mode: production ? "production" : "development",
+  devtool: production ? "source-map" : "eval-source-map",
 };
 
 module.exports = config;
